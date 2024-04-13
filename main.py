3b4f27f924c2381b006ea508201099fd4a7f13e2
zliddle <166666647+zliddle@users.noreply.github.com>
2024-04-13 11:05:26 -0400

    Zilong Liddle has added the decode function

--- main.py
+++ main.py
@@ -13,23 +13,34 @@ def encoder(password1):
         encoded_digits.append(str(new_digit))
 
     encoded_password = ''.join(encoded_digits)
-    print("Your password has been encoded and stored!")
 
     return encoded_password
 
+def decode(newstring):
+    newlist = []
+    decodedstring = ""
+    for i in range(len(newstring)):
+        newlist.append(int(newstring[i]))
+    final_list = [sublist - 3 for sublist in newlist]
+    for i in range(len(final_list)):
+        decodedstring = decodedstring + str(final_list[i])
+    return decodedstring
 
 def main():
-    print("Menu")
-    print("-------------")
-    choice = int(input("1. Encode\n2. Decode\n3. Quit\n"))
+    choice = 0
+    while choice != 3:
+        print("Menu")
+        print("-------------")
+        choice = int(input("1. Encode\n2. Decode\n3. Quit\n"))
 
-
-    if choice == 1:
-        encoder(input("Please enter your password to encode: "))
-    elif choice == 2:
-        pass
-    elif choice == 3:
-        quit()
+        if choice == 1:
+            encoded_password = encoder(input("Please enter your password to encode: "))
+        elif choice == 2:
+            decodedstring = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decodedstring}\n")
+            pass
+        elif choice == 3:
+            quit()
 
 
 if __name__ == "__main__":
